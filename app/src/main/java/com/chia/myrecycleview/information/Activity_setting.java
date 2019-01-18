dc04c5f53ed5ccbbdee4682b080608109c7e1c35
aceg224 <aceg224@gmail.com>
2019-01-18 17:49:00 +0800

    change Layout

--- app/src/main/java/com/chia/myrecycleview/information/Activity_setting.java
+++ app/src/main/java/com/chia/myrecycleview/information/Activity_setting.java
@@ -3,12 +3,10 @@ package com.chia.myrecycleview.information;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.chia.myrecycleview.R;
+
 public class Activity_setting extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
     }
 
-}
